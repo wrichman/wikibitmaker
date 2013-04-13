25a1dba6da44804b70de7ad69a2b92942da7de38
Matt Vleming <mattvleming@gmail.com>
2013-04-13 15:30:55 -0400

    I don't want to name thsi commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,29 @@
 class ArticlesController < ApplicationController
-	def index
-    @article = Article.find_by_title('Home')
+	before_filter :require_article, only: [:show, :edit, :update]
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @article.update_attributes params[:article]
+			redirect_to @article, notice: "You've improved the article."
+		else
+			render :new
+		end
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	protected 
+
+	def require_article
+		@article = Article.find params[:id]
 	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < ApplicationController
 
 	def create
 		@user = login(params[:username], params[:password])
+		@article = Article.find_by_title('Home')
 		if @user
-			redirect_to articles_path, :notice => "Welcome to the exclusive Wiki for Bitmaker Labs."
+			redirect_to article_path(@article), :notice => "Welcome to the exclusive Wiki for Bitmaker Labs."
 		else
 			flash.now[:alert] = "Invalid username or password."
 			render :new
